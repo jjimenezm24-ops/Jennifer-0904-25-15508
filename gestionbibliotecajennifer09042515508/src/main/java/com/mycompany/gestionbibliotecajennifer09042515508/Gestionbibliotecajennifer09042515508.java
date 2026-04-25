97cef075712905366f14aadfcf15b437b2392da5
umg <umg@HUE-LAB-1E1.labhue.local>
2026-04-25 09:51:11 -0600

    He creado el proyecto y su estructura de paquetes

---
+++ gestionbibliotecajennifer09042515508/src/main/java/com/mycompany/gestionbibliotecajennifer09042515508/Gestionbibliotecajennifer09042515508.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestionbibliotecajennifer09042515508;
+
+/**
+ *
+ * @author umg
+ */
+public class Gestionbibliotecajennifer09042515508 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
